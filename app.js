27ddbc42c806f0664fb123d2c80f38ad7a25f9af
Alexis Gonzalez <a.rgonzalez0951@gmail.com>
2024-02-23 20:25:44 -0800

    updated code

---
+++ app.js
@@ -0,0 +1,9 @@
+// The forecast today in kelvin
+const kelvin = 293;
+
+// Conversion from kelvin to celsius
+const celsius = kelvin - 273;
+
+// Conversion from celsius to fahrenheit
+const fahrenheit = celsius * (9/5) + 32;
+
